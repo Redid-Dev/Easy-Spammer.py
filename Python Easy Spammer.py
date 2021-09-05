9e143f5d9e6f693aee2053c572603006590c9a3e
Redid <74817969+Redid-Dev@users.noreply.github.com>
2021-09-05 12:37:55 +0200

    Add files via upload

---
+++ Python Easy Spammer.py
@@ -0,0 +1,13 @@
+import pyautogui as auto
+
+print ("=======================")
+print ("| Redid Easy Spammer |")
+print ("=======================")
+
+time:auto.sleep(1)
+while True:
+    auto.write('INSERT HERE TEXT')
+    auto.press('enter')
+    time:auto.sleep(1)
+
+    
