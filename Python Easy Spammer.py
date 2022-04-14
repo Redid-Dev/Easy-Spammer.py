1074c6c6c35f6200f52ad3f169562c45b9e8a0e0
qweryy <74817969+qweryydev@users.noreply.github.com>
2022-04-14 22:18:48 +0200

    EasySpammer V2 released

--- Python Easy Spammer.py
+++ Python Easy Spammer.py
@@ -1,13 +1,33 @@
-import pyautogui as auto
+#========================================================================================#
+# ______                 _____                                            __      _____  #                                                               
+#|  ____|               / ____|                                           \ \    / /__ \ #            2022 Copyright (c) qweryy-dev                 
+#| |__   __ _ ___ _   _| (___  _ __   __ _ _ __ ___  _ __ ___   ___ _ __   \ \  / /   ) |#            2022 Copyright (c) EasySpammerV2                   
+#|  __| / _` / __| | | |\___ \| '_ \ / _` | '_ ` _ \| '_ ` _ \ / _ \ '__|   \ \/ /   / / #            2021 Copyright (c) EasySpammer          
+#| |___| (_| \__ \ |_| |____) | |_) | (_| | | | | | | | | | | |  __/ |       \  /   / /_ #                      
+#|______\__,_|___/\__, |_____/| .__/ \__,_|_| |_| |_|_| |_| |_|\___|_|        \/   |____|#                                                               
+#                  __/ |      | |                                                        #                                                               
+#                 |___/       |_|                                                        #                                                               
+#========================================================================================#
 
-print ("=======================")
-print ("| ForseeGab Easy Spammer |")
-print ("=======================")
+print("==========================")
+print("|EasySpammer V2 by qweryy|")
+print("===========================")
 
-time:auto.sleep(1)
-while True:
-    auto.write('INSERT HERE TEXT')
-    auto.press('enter')
-    time:auto.sleep(1)
+import random
+import pyautogui as pg
+import time
+#🔤USE FOR WORD
+word=('PASTE/TYPE THE WORD HERE')
+#🔗USE FOR LINKS
+link=('PASTE/TYPE THE LINK HERE')
 
+#⌚DELAY (by changing the number (default 8) the delay will change)
+time.sleep(8)
+
+#🔢WORD/LINK REPEATER
+for i in range(100):
+#📥INPUT
+    pg.write(word)
+    pg.press('enter')
+    
     
